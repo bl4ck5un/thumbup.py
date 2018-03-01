e53ce2046339155ce148695b9f7fc601928f55fe
Fan Zhang <bl4ck5unxx@gmail.com>
2018-03-01 15:38:33 -0400

    bump version to v0.1.1

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='thumbup',
-    version='0.1.0',
+    version='0.1.1',
     description='a convenient video thumbnails generator, based on ffmpeg',
     long_description=long_description,
     url='https://github.com/bl4ck5un/thumbup.py',
